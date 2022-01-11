5e5d5b6f69ecc514933a87d3577d4e34a9f98e84
Dave <david.mathaga@student.moringaschool.com>
2022-01-11 09:53:28 +0300

    tests

--- neighbouhood/settings.py
+++ neighbouhood/settings.py
@@ -9,7 +9,9 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
+import django_heroku
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
