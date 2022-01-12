4b3d525696ab28f843b11539fcc0eeb2138dcc96
Dave <david.mathaga@student.moringaschool.com>
2022-01-12 06:55:32 +0300

    settings editing

--- neighbouhood/settings.py
+++ neighbouhood/settings.py
@@ -16,10 +16,12 @@ import dj_database_url
 from decouple import config,Csv
 from pathlib import Path
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+
 
 
 # Quick-start development settings - unsuitable for production
